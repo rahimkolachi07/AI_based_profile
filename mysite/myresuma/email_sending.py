c8ab9e7ce1c5b1bba5bbdae8619c42067693f8af
rahimkolachi07 <raheemkolachi7@gmail.com>
2024-05-20 20:37:15 -0700

    updated

--- mysite/myresuma/email_sending.py
+++ mysite/myresuma/email_sending.py
@@ -1,8 +1,18 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import re
+
+def remove_text_in_brackets(text):
+    # This regular expression matches any text between square brackets
+    pattern = r'\[.*?\]'
+    # Substitute the matched text with an empty string
+    cleaned_text = re.sub(pattern, '', text)
+    return cleaned_text
 
 def email_sending(receiver,text,subject):
+    text=remove_text_in_brackets(text)
+    subject=remove_text_in_brackets(subject)
     email_content = text
     sender_email = "rahimkolachi16@gmail.com"
     receiver_email = receiver
